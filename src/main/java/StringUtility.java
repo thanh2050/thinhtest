5d421ccce41d348419121db58a39a833e5f86174
thanh <nguyenducthanh2050@gmail.com>
2017-09-17 17:16:59 +0700

    - init project

---
+++ src/main/java/StringUtility.java
@@ -0,0 +1,65 @@
+public class StringUtility {
+
+    public static int NumberTimeAppearance(String parrent, String child)
+    {
+        int result = 0;
+        try
+        {
+            int numberToShow = 0;
+            int theLastIndex = 0;
+            boolean isDone = false;
+
+            do {
+                int temp = parrent.toLowerCase().indexOf(child.toLowerCase(),theLastIndex + 1);
+                if(temp != theLastIndex && temp != - 1)
+                {
+                    theLastIndex = temp;
+                    numberToShow ++;
+                }
+                else
+                {
+                    isDone = true;
+                }
+            }
+            while (!isDone);
+
+            result = numberToShow;
+        }
+        catch (Exception ex)
+        {
+            // log error
+        }
+        return result;
+    }
+
+    public static int NumberTimeAppearance2(String parrent, String child)
+    {
+        int result = 0;
+        try
+        {
+            int numberToShow = 0;
+            for (int i = 0; i < parrent.length(); i++)
+            {
+                char character = parrent.toLowerCase().charAt(i);
+                char firstChildChar = child.toLowerCase().charAt(0);
+                if(firstChildChar == character)
+                {
+                    if(i + child.length() <= parrent.length())
+                    {
+                        String temp = parrent.toLowerCase().substring(i, i + child.length());
+                        if(temp.equals(child.toLowerCase()))
+                        {
+                            numberToShow ++;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // log error
+        }
+        return result;
+    }
+
+}
